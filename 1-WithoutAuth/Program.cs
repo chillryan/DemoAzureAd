3f90938763d4ad6c17a49d407e0f1660257b59c1
Ryan Hill <ryanchill@hotmail.com>
2019-09-11 16:45:09 -0400

    adding appinsights

--- 1-WithoutAuth/Program.cs
+++ 1-WithoutAuth/Program.cs
@@ -19,6 +19,7 @@ namespace DemoAzureAd.Site
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
